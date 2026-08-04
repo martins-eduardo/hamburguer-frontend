c6d954378f727248594a26c162b72b0efc76eda0
Eduardo Martins <eduardo.martins22@aluno.senai.br>
2026-08-03 22:24:15 -0300

    chamada de sign in

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,11 +1,17 @@
+import { useFormContext } from 'react-hook-form'
+
 interface InputProps {
    placeholder: string
    type: string
+   name: string
 }
 
-export function Input({ placeholder, type }: InputProps) {
+export function Input({ placeholder, type, name }: InputProps) {
+   const { register } = useFormContext()
+
    return (
       <input
+         {...register(name)}
          className="w-87.5 h-9 bg-white p-2 outline-none rounded-sm text-[#32343E] placeholder-[#32343E]"
          placeholder={placeholder}
          type={type}
--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -1,23 +1,70 @@
 import { useNavigate } from 'react-router-dom'
+import { FormProvider, useForm } from 'react-hook-form'
 import { Input } from '../../components/Input'
+import { api } from '../../lib/api'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useState } from 'react'
+
+export const signInSchema = yup.object().shape({
+   email: yup.string().email().required('Required field'),
+   password: yup.string().required('Required field'),
+})
+
+type SignInSchema = yup.InferType<typeof signInSchema>
 
 export function SignIn() {
+   const signInForm = useForm({
+      resolver: yupResolver(signInSchema),
+   })
+
+   const { handleSubmit } = signInForm
+
+   const [loading, setLoading] = useState(false)
+
    const navigate = useNavigate()
+
+   async function handleSignSubmit(data: SignInSchema) {
+      try {
+         setLoading(true)
+
+         const response = await api.post('/sign-in', {
+            email: data.email,
+            password: data.password,
+         })
+
+         console.log(response.data)
+      } catch (error) {
+         console.error(error)
+      } finally {
+         setLoading(false)
+      }
+   }
+
    return (
       <div className="flex flex-col h-full justify-center items-center gap-6">
          <img src="./hamburguer-logo.png" alt="" />
-         <div className="flex flex-col gap-2">
-            <Input placeholder="E-mail" type="text" />
-            <Input placeholder="Senha" type="password" />
-         </div>
+         <FormProvider {...signInForm}>
+            <form
+               id="sign-in-form"
+               className="flex flex-col gap-2"
+               onSubmit={handleSubmit(handleSignSubmit)}
+            >
+               <Input name="email" placeholder="E-mail" type="text" />
+               <Input name="password" placeholder="Senha" type="password" />
+            </form>
+         </FormProvider>
          <div className="flex flex-col gap-2 max-w-87.5 w-full">
             <button
+               form="sign-in-form"
+               disabled={loading}
                type="submit"
                className="flex bg-[#C92A0E] rounded-sm h-9 items-center justify-center text-white font-semibold hover:cursor-pointer"
             >
                Login
             </button>
             <button
+               disabled={loading}
                type="button"
                className="flex bg-white border-2 border-[#C92A0E] text-[#C92A0E] font-semibold rounded-sm h-9 items-center justify-center hover:cursor-pointer"
                onClick={() => navigate('/sign-up')}
